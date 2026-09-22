d61e71afc83ead584487cd8e7497718dd24fa407
sudheer-naidu <sudheersattineedi99@gmail.com>
2026-09-22 14:55:05 +0530

    Auth service added

--- soa-project/room-service/src/main/java/com/starlight/room/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.starlight.room.repository;
-
-import com.starlight.room.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
